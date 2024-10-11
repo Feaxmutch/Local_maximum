260d633672ff0c4cd476e768e6796008053f4ea7
Feaxmutch <feaxmutch@gmail.com>
2024-10-11 14:15:14 +0300

    Implemented searching of the local maximum

--- LocalMaximum/Program.cs
+++ LocalMaximum/Program.cs
@@ -4,7 +4,48 @@
     {
         static void Main(string[] args)
         {
-            
+            Random random = new();
+            int minNumber = 0;
+            int maxNumber = 9;
+            int[] numbers = new int[30];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(minNumber, maxNumber + 1);
+                Console.Write($"{numbers[i]} ");
+            }
+
+            Console.WriteLine("\n\nВсе локальные максимумы:");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (i > 0)
+                {
+                    if (i < numbers.Length - 1)
+                    {
+                        if (numbers[i] > numbers[i + 1] && numbers[i] > numbers[i - 1])
+                        {
+                            Console.Write($"{numbers[i]} ");
+                        }
+                    }
+                    else
+                    {
+                        if (numbers[i] > numbers[i - 1])
+                        {
+                            Console.Write($"{numbers[i]} ");
+                        }
+                    }
+                }
+                else
+                {
+                    if (numbers[i] > numbers[i + 1])
+                    {
+                        Console.Write($"{numbers[i]} ");
+                    }
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
